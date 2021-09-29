78c3466a1fc9d154ab160adfb10eaf0b57e83e91
mytrofanov <mytrofanovmaxym@gmail.com>
2021-09-29 15:40:34 +0300

    Создал структуру профайла

--- src/components/portfolio/Work.js
+++ src/components/portfolio/Work.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import {Card, Grid, CardMedia, CardContent, Typography} from "@material-ui/core";
+import {Card, Grid, CardMedia, CardContent, Typography, CardActions} from "@material-ui/core";
+import Button from "@mui/material/Button";
 
 
 export const Work = (props) => {
+    const ref = props.reference
     return (
         <Grid item xs={12} md={12}>
             <Card>
                 <CardMedia
                     src={props.image}
+                    component="img"
                     title={props.id}
-                    sx={{height: 140}}
+                    sx={{width: 700}}
                     id={props.image} alt={props.id}/>
                 <CardContent>
                     <Typography variant="h5"
                     component="h3">{props.name}</Typography>
                     <Typography variant="h6" >{props.description}</Typography>
-                    <Typography variant="body1">{props.reference}</Typography>
+                    <CardActions>
+                    <Button
+                        value="<?php the_field('Открыть проект');?>"
+                        onClick={()=>{window.open(ref)}}
+                         >Открыть проект</Button>
+                    </CardActions>
                 </CardContent>
             </Card>
         </Grid>
