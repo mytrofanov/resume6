f011fd92daedc119c870b01b38dfca17990df2b3
mytrofanov <mytrofanovmaxym@gmail.com>
2021-09-29 10:58:02 +0300

    Создал структуру

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import * as React from 'react';
+import './App.css';
 import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
-import {AppHeader} from "./components/Header";
-
+import portrait from "./images/portrait_cr.jpg"
 
 export default function App() {
     return (
         <ScopedCssBaseline>
-            <div>
-                <AppHeader/>
+            <div className="container">
+
+                <div className="navbar">
+                    <img alt="Mytrofanov Maxym" src={portrait} id="portrait"/>
+
+                    <div id="navi_list">
+                        <li id="Home">Home</li>
+                        <li id="About">About me</li>
+                        <li id="Education">Education</li>
+                        <li id="Experience">Work Experience</li>
+                        <li id="Skills">Skills</li>
+                        <li id="Portfolio">Portfolio</li>
+                        <li id="Contacts">Contacts</li>
+
+                    </div>
+                </div>
+
+                <div className="right_column" id="right_column">
+                    <div className="header" id="header">
+                        <div id="header_title">
+                            Lets Make Something Great
+                            <div className="header_annotation" id="header_annotation">
+                                I’m an UI designer that prides ourself on being strategic brand partners and awesome
+                                product providers
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="portfolio_block" id="portfolio_block">
+                        <span className="portfolio_block_span">Portfolio</span>
+                        <div className="portfolio_inner_block">
+                            portfolio_inner_block
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </ScopedCssBaseline>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {AppBar} from 'material-ui';
 
 
 export const AppHeader = () => {
 
     return (
         <div>
-            <AppBar title="Title"/>
+
         </div>
     )
 }
