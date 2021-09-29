6f2587fbcf6abc737ec61d33c5352bd930b290cd
mytrofanov <mytrofanovmaxym@gmail.com>
2021-09-29 10:00:53 +0300

    Установил @material/top-app-bar  - библиотека для header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import Button from '@material-ui/core/Button';
 import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
+import {AppHeader} from "./components/Header";
+
 
 export default function App() {
     return (
         <ScopedCssBaseline>
-        <Button variant="contained" color="primary">
-            Hello world
-        </Button>
+            <div>
+                <AppHeader/>
+            </div>
         </ScopedCssBaseline>
     )
 }
