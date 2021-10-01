0fdd767ce665c1fb797f5a7e356a58ed4610ba7f
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-01 09:07:49 +0300

    Изменил описание проектов

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -7,7 +7,7 @@ export const MyWorks = [{
     id: 'Resume',
     image: samurai,
     name: 'Resume',
-    description: 'This site was made using HTML5, CSS3, React, JavaScript, Material UI, Git',
+    description: 'This site was made using HTML5, CSS3, React, JavaScript, Material UI, react-hook-form, Git',
     reference: 'https://mytrofanov.github.io/second/#/profile'
 }, {
     id: 'Landing Page',
@@ -19,7 +19,8 @@ export const MyWorks = [{
     id: 'Social Network',
     image: samurai,
     name: 'The social-network website',
-    description: 'This Social Network was build using React, Redux, JavaScript, TypeScript, CSS, Ajax, API, Login with cookie, Git',
+    description: 'This Social Network was build using React, Redux, JavaScript, TypeScript, CSS, Ajax, API, ' +
+        'cookie, react-hook-form, Git',
     reference: 'https://mytrofanov.github.io/second/#/profile'
 }
 ]
