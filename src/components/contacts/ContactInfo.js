15574289369a572c4dad7685ae8bdba9d0e449c4
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-02 23:20:38 +0300

    Установил пакет @material-ui/icons

--- src/components/contacts/ContactInfo.js
+++ src/components/contacts/ContactInfo.js
@@ -4,7 +4,7 @@ export function ContactInfo() {
     return (
         <div>
             <div className={s.ContactInfo}>
-                <div className={s.icon}>Icon</div>
+                <div className={s.icon}>Icon </div>
                 <div>Ukraine, Kryvyi Rih city, 50082</div>
             </div>
 
