8ce273ec528eb80888f3cab72ad0b4df8f1f0a05
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-04 16:03:39 +0300

    Добавил rel="noreferrer" в ссылки

--- src/components/bottom/Bottom.js
+++ src/components/bottom/Bottom.js
@@ -7,7 +7,7 @@ export function Bottom() {
     return (
         <div className={s.Bread}>
             <div className={s.BreadPieces}>
-                <a rel="nofollow" target="_blank" href="https://www.facebook.com/profile.php?id=100005637646093">
+                <a rel="noreferrer" target="_blank" href="https://www.facebook.com/profile.php?id=100005637646093">
                     <FacebookIcon
                         sx={{
                             fontSize: 30,
@@ -16,7 +16,7 @@ export function Bottom() {
                 </a>
             </div>
             <div className={s.BreadPiecesMid}>
-                <a rel="nofollow" target="_blank" href="https://www.instagram.com/mytrofanovmaxym/?hl=ru">
+                <a rel="noreferrer" target="_blank" href="https://www.instagram.com/mytrofanovmaxym/?hl=ru">
                 <InstagramIcon
                     sx={{
                         fontSize: 30,
@@ -25,7 +25,7 @@ export function Bottom() {
                 </a>
             </div>
             <div className={s.BreadPieces}>
-                <a rel="nofollow" target="_blank" href="https://www.linkedin.com/in/maksym-mytrofanov-967365215/">
+                <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/maksym-mytrofanov-967365215/">
                 <LinkedInIcon
                     sx={{
                         fontSize: 30
