4bf7796fde60026099f1061cce8af71c6e18563b
mytrofanov <mytrofanovmaxym@gmail.com>
2022-04-08 12:32:34 +0300

    alert translate

--- src/components/aletrMessage/AlertMessage.js
+++ src/components/aletrMessage/AlertMessage.js
@@ -1,15 +1,20 @@
 import Stack from "@mui/material/Stack";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
-import React from "react";
+import React, {useContext} from "react";
+import {Context} from "../../App";
 
 
 
 export const AlertMessage = () => {
+    let language = useContext(Context)
     return (
         <Stack sx={{width: '100%'}} spacing={2}>
             <Alert severity="success" variant="filled">
-                <AlertTitle> Your message has been send</AlertTitle>
+                {language === 'English'&& <AlertTitle> Your message has been send</AlertTitle> }
+                {language === 'Ukrainian'&& <AlertTitle> Ваше повідомлення відправлено</AlertTitle> }
+
+
 
             </Alert>
         </Stack>
--- src/components/aletrMessage/ErrorAlertMessage.js
+++ src/components/aletrMessage/ErrorAlertMessage.js
@@ -1,17 +1,24 @@
 import Stack from "@mui/material/Stack";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
-import React from "react";
+import React, {useContext} from "react";
+import {Context} from "../../App";
 
 
 
 export const ErrorAlertMessage = ({errorText}) => {
+    let language = useContext(Context)
     return (
         <Stack sx={{width: '100%'}} spacing={2}>
             <Alert severity="error" variant="filled">
-                <AlertTitle> Your message was not send</AlertTitle>
-                Reason: {errorText}
-                You can get me on e-mail:  <strong>MytrofanovMaxym@gmail.com</strong>
+                {language === 'English'&& <> <AlertTitle> Your message was not send</AlertTitle>
+                    Reason: {errorText}
+                    You can get me on e-mail:  <strong>MytrofanovMaxym@gmail.com</strong></> }
+                {language === 'Ukrainian'&&<> <AlertTitle> Ваше повідомлено не відправлено</AlertTitle>
+                    Причина: {errorText}
+                    Можна зв'язатися зі мною по e-mail:  <strong>MytrofanovMaxym@gmail.com</strong></> }
+
+
             </Alert>
         </Stack>
     );
